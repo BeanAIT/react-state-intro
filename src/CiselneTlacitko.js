0a8773dbb433a62526cc15bad05a30273ff89ab1
Václav Jonák <vaclav.jonak@beanstudent.cz>
2023-12-19 12:32:41 +0100

    První funkční verze kalkulačky

--- src/CiselneTlacitko.js
+++ src/CiselneTlacitko.js
@@ -1,6 +1,18 @@
 function CiselneTlacitko(props) {
-return (
-    <button>{props.cislo}</button>
+    let puvodniCislo = props.puvodniCislo;
+    let cisloTlacitka = props.cislo
+    let vysledek = puvodniCislo.toString().concat(props.cislo);
+
+    function nastavCislo() {
+        if(puvodniCislo == "0") {
+            props.klikFunkce(cisloTlacitka)
+        } else {
+            props.klikFunkce(vysledek)
+        }
+    }
+    return (
+    
+    <button onClick={() => nastavCislo()}>{props.cislo}</button>
 )
 
 
--- src/Kalkulacka.js
+++ src/Kalkulacka.js
@@ -1,25 +1,63 @@
+
+import { useState } from "react";
 import CiselneTlacitko from "./CiselneTlacitko";
-
+import './Kalkulacka.css';
 function Kalkulacka() {
-return (
-    <div>
-<CiselneTlacitko cislo={7}/>
-<CiselneTlacitko cislo={8}/>
-<CiselneTlacitko cislo={9} />
-<br/>
-<CiselneTlacitko cislo={4}/>
-<CiselneTlacitko cislo={5}/>
-<CiselneTlacitko cislo={6}/>
-<br/>
-<CiselneTlacitko cislo={1}/>
-<CiselneTlacitko cislo={2}/>
-<CiselneTlacitko cislo={3}/>
-<br/>
-<CiselneTlacitko cislo={0}/>
+
+    const [cislo, nastavCislo] = useState("0");
+    const [operace, nastavOperaci] = useState("");
+    const [prvniCislo, nastavPrvniCislo] = useState(0);
+
+function vybranaOperace(symbol) {
+    nastavOperaci(symbol)
+    nastavPrvniCislo(cislo)
+    nastavCislo("0")
+}
+
+    function spocitejVysledek() {
+       let vysledek
+       let prvniCisloNum = parseInt(prvniCislo)
+        if (operace == "+") {
+vysledek = prvniCisloNum + cislo
+        } else if (operace == "-") {
+            vysledek = prvniCisloNum - cislo
+        } else if (operace == "*") {
+            vysledek = prvniCisloNum * cislo
+        } else if (operace == "/") {
+            vysledek = prvniCisloNum / cislo
+        }
+        nastavCislo(vysledek)
+    }
+    return (
+        <>
+            <div id="kalkulacka">
+                <div id="displej">{cislo}</div>
+                <div id="tlacitka">
+                    <CiselneTlacitko cislo={7} klikFunkce={nastavCislo} puvodniCislo={cislo}/>
+                    <CiselneTlacitko cislo={8} klikFunkce={nastavCislo} puvodniCislo={cislo}/>
+                    <CiselneTlacitko cislo={9} klikFunkce={nastavCislo} puvodniCislo={cislo}/>
+                    <button onClick={() => vybranaOperace("+")}>+</button>
+                    <br />
+                    <CiselneTlacitko cislo={4} klikFunkce={nastavCislo} puvodniCislo={cislo}/>
+                    <CiselneTlacitko cislo={5} klikFunkce={nastavCislo} puvodniCislo={cislo}/>
+                    <CiselneTlacitko cislo={6} klikFunkce={nastavCislo} puvodniCislo={cislo}/>
+                    <button onClick={() => vybranaOperace("-")}>-</button>
+                    <br />
+                    <CiselneTlacitko cislo={1} klikFunkce={nastavCislo} puvodniCislo={cislo}/>
+                    <CiselneTlacitko cislo={2} klikFunkce={nastavCislo} puvodniCislo={cislo}/>
+                    <CiselneTlacitko cislo={3} klikFunkce={nastavCislo} puvodniCislo={cislo}/>
+                    <button onClick={() => vybranaOperace("*")}>*</button>
+                    <br />
+                    <button onClick={() => nastavOperaci("C")}>C</button>
+                    <CiselneTlacitko cislo={0} klikFunkce={nastavCislo} puvodniCislo={cislo}/>
+                    <button onClick={() => nastavOperaci("=")}>=</button>
+                    <button onClick={() => vybranaOperace("/")}>/</button>
 
 
-    </div>
-)
+                </div>
+            </div>
+        </>
+    )
 
 }
 
